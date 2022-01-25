04815add29a340e83f88b6cc79e0d5635dd0b333
Inhyeok Bang <bih3754@gmail.com>
2022-01-25 22:19:34 +0900

    Update array.h (Modifying array.cc is going on...)

--- Array/array.h
+++ Array/array.h
@@ -11,31 +11,32 @@
 
 class Array {
  public:
-  explicit Array(Array* parent, int val, int idx)
-      : arr_(nullptr), parent_(parent), val_(val), depth_(0), idx_(idx) {}
-  Array(Array* parent, int* widths, int depth, int idx)
-      : parent_(parent), depth_(depth), idx_(idx) {
-    for (int i = 0; i < *widths; ++i) {
-      arr_ = new Array(this, widths + 1, depth - 1, i);
-    }
+  Array(int* size, int dim) : size_(size), dimension_(dim) {
+    head_ = new Node;
+    InitNode(head_);
   }
-  Array(int* widths, int depth) : parent_(nullptr), depth_(depth), idx_(-1) {
-    for (int i = 0; i < *widths; ++i) {
-      arr_ = new Array(this, widths + 1, depth - 1, i);
-    }
-  }
-
-  Array& operator=(Array* arr);
-  Array& operator=(int val);
-  void* operator[](int idx);
 
  private:
-  Array* arr_;
-  Array* parent_;
-  int val_;
-  int width_;
-  int depth_;
-  int idx_;
+  struct Node {
+    int level_;
+    void* next_;
+  };
+  Node* head_;
+  int dimension_;
+  int* size_;
+
+  void InitNode(Node* cur) {
+    int size = *(size_ + dimension_ - cur->level_);
+    if (cur->level_ == 1) {
+      cur->next_ = new int[size];
+    } else {
+      cur->next_ = new Node[size];
+      for (int i = 0; i < size; ++i) {
+        static_cast<Node*>(cur->next_ + i)->level_ = cur->level_ - 1;
+        InitNode(static_cast<Node*>(cur->next_ + i));
+      }
+    }
+  }
 };
 
 #endif  // CPP_PRACTICE_ARRAY_H_
